23e2821dd1790966819bef0a18e0e1dff19d67b1
✪Ohmcodes™✪ <ohmcodes@gmail.com>
2024-06-08 15:45:23 -0700

    Update Soulmask.cs
    Added safe save world when closing/shutting down the server

--- Soulmask.cs/Soulmask.cs
+++ Soulmask.cs/Soulmask.cs
@@ -130,6 +130,8 @@ namespace WindowsGSM.Plugins
             await Task.Run(() =>
             {
                 Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
+                Functions.ServerConsole.SendWaitToMainWindow("quit 10");
+                Task.Delay(11000);
                 Functions.ServerConsole.SendWaitToMainWindow("^c");
             });
             await Task.Delay(2000);
